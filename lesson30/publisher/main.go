85829ae802d319b3231f16e216c9f443ad0a364d
asliwebdev <asliddiniut@gmail.com>
2025-01-12 21:53:04 +0500

    added pub-sub

---
+++ lesson30/publisher/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	// Create a new Redis client
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // Adjust the address if necessary
+		Password: "",               // No password set
+		DB:       0,                // Use default DB
+	})
+
+	ctx := context.Background()
+	channel := "mychannel"
+
+	// Publish messages to the channel every second
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for t := range ticker.C {
+		message := fmt.Sprintf("Current time: %s", t.Format(time.RFC3339))
+		err := rdb.Publish(ctx, channel, message).Err()
+		if err != nil {
+			fmt.Println("Error publishing message:", err)
+		} else {
+			fmt.Println("Published message:", message)
+		}
+	}
+}
